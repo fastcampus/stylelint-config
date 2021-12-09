70833d960cc7a3bde10f089bc54b83bb2cae71c1
Jaewon-Noh <jwno@fastcampus.co.kr>
2021-12-09 16:22:54 +0900

    슬랙 훅 url변경

--- .github/npm-publish-slack.sh
+++ .github/npm-publish-slack.sh
@@ -4,7 +4,7 @@ slackToDeptDev() {
 	curl \
 			-X POST \
 			--data-urlencode \
-			"payload={\"channel\": \"#npm-publish-notify\", \"text\": \"$1\", \"attachments\":[{ \"fields\":[{\"title\":\"Notes\",\"value\":\"$2\"  }] }] }" https://hooks.slack.com/services/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
+			"payload={\"channel\": \"#npm-publish-notify\", \"text\": \"$1\", \"attachments\":[{ \"fields\":[{\"title\":\"Notes\",\"value\":\"$2\"  }] }] }" https://hooks.slack.com/services/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
 }
 
 PACKAGE_NAME=$(jq -r .name package.json)
